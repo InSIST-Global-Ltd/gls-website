228d6f2cf5fa32a48b1c25724212c7cf19829361
Jainaba Faal <jfaal@insistglobal.com>
2024-07-08 12:49:24 +0000

    update build number

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t  insistglobal/lecket:gls-website-002 .
-docker push insistglobal/lecket:gls-website-002
+docker build -t  insistglobal/lecket:gls-website-003 .
+docker push insistglobal/lecket:gls-website-003
