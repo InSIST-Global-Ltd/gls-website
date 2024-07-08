223e436473585717fbc86f15a4c7935fd3ad4fe5
Jainaba Faal <jfaal@insistglobal.com>
2024-07-08 14:27:28 +0000

    add call to action file and update build number

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t  insistglobal/lecket:gls-website-003 .
-docker push insistglobal/lecket:gls-website-003
+docker build -t  insistglobal/lecket:gls-website-004 .
+docker push insistglobal/lecket:gls-website-004
