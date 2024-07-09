e826c2fae2badc8e60dd7067eaa685362ad0fb1b
mbanyick <73003805+mbanyick@users.noreply.github.com>
2024-07-09 12:58:28 +0300

    update build to 6

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t  insistglobal/lecket:gls-website-005 .
-docker push insistglobal/lecket:gls-website-005
+docker build -t  insistglobal/lecket:gls-website-006 .
+docker push insistglobal/lecket:gls-website-006
