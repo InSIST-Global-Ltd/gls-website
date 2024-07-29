75cd113b0f772aa57c020f7482c6342c892ce19e
Jainaba Faal <jfaal@insistglobal.com>
2024-07-29 16:54:53 +0000

    add notice section and update build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t  insistglobal/lecket:gls-website-006 .
-docker push insistglobal/lecket:gls-website-006
+docker build -t  insistglobal/lecket:gls-website-009 .
+docker push insistglobal/lecket:gls-website-009
