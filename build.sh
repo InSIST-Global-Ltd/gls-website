0720888ab94da6b396f902a4656b5b5bbae1225c
Jainaba Faal <jfaal@insistglobal.com>
2024-09-02 14:42:10 +0000

    remove admission button and all text related to admission

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t  insistglobal/lecket:gls-website-014 .
-docker push insistglobal/lecket:gls-website-014
+docker build -t  insistglobal/lecket:gls-website-015 .
+docker push insistglobal/lecket:gls-website-015
